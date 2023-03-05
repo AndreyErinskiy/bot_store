dadb9ee600c8533920a0be2f2268525f7c6296d6
Андрей <vintag_vintag@mail.ru>
2023-03-05 21:49:20 +0700

    next 1

--- create_bot.py
+++ create_bot.py
@@ -1,5 +1,9 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+storage = MemoryStorage()
 
 bot = Bot(token='5903774545:AAFZMLBH3rbzMjjR2r2Vb_W10sgabK97EW0')
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
+
--- handlers/client.py
+++ handlers/client.py
@@ -1,27 +1,34 @@
-from aiogram import types
-from bot_telegram import dp
+from aiogram import types, Dispatcher
 from create_bot import bot
+from keyboards import kb_client
 
 
-@dp.message_handler(commands=['start', 'help'])
+# @dp.message_handler(commands=['start', 'help'])
 async def command_start(message: types.Message):
     try:
-        await bot.send_message(message.from_user.id, 'Хороших покупок)')
+        await bot.send_message(message.from_user.id, 'Хороших покупок)', reply_markup=kb_client)
         await message.delete()
     except:
         await message.reply('Общение с ботом через Лс, напишите ему :\nhttps://t.me/posh_nvkz_bot')
 
 
-@dp.message_handler(commands=['rules'])
+# @dp.message_handler(commands=['rules'])
 async def order_conditions(message: types.Message):
     await bot.send_message(message.from_user.id, 'ПРОПИСАТЬ УСЛОВИЯ ДЛЯ ЗАКАЗА')
 
 
-@dp.message_handler(commands=['Реквизиты'])
+# @dp.message_handler(commands=['Реквизиты'])
 async def money_send(message: types.Message):
     await bot.send_message(message.from_user.id, 'ПРОПИСАТЬ РЕКВИЗТЫ ДЛЯ ПРИЕМА ДЕНЕГ')
+
 
 # @dp.message_handler(commands=['Меню'])
 # async def shop_menu_commands(message : types.Message):
 #     for ret in cur.execute('SELECT * FROM menu').fetchall():
 #         await bot.send_message(message.from_user.id, ret[0], f'{ret[1]}\nОписание: {ret[2]}\nЦена {ret[-1]}')
+
+
+def register_handlers_client(dp: Dispatcher):
+    dp.register_message_handler(command_start, commands=['start', 'help'])
+    dp.register_message_handler(order_conditions, commands=['rules'])
+    dp.register_message_handler(money_send, commands=['Реквизиты'])
